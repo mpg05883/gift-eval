3e07c9b386540fd8cc03902e07f96d75de062b3a
mpg05883 <mpgee@usc.edu>
2026-09-14 20:20:40 -0500

    Updated code

--- scripts/get_metadata.py
+++ scripts/get_metadata.py
@@ -1,15 +1,15 @@
 import argparse
 from pathlib import Path
-
+from src.gift_eval.data import Dataset
 
 def main(args: argparse.Namespace):
-    root = Path(__file__).resolve().parents[2]
+    root = Path(__file__).resolve().parents[1]
     data_dir = root / "data" / args.split
-    for dataset in data_dir.glob("*"):
-        if dataset.is_dir() and dataset != ".cache":
-            print(dataset.name)
-
-
+    names = [d.name for d in data_dir.iterdir() if d.is_dir() and d.name != ".cache"]
+    names = sorted(names, key=lambda x: x.lower())
+    datasets = [Dataset(name) for name in names]
+    print(f"Number of datasets: {len(datasets)}")
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--split", type=str, default="GiftEvalPretrain")
